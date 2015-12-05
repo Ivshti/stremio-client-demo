c08b483059b84739a0ee8125bc6ece1125189882
Ivo Georgiev <ivo@linvo.org>
2015-12-05 17:29:33 +0200

    Load all types of content

--- public/catalog.js
+++ public/catalog.js
@@ -6,22 +6,26 @@ Catalog.config([ '$compileProvider', function($compileProvider) {
 }]);
 
 // Initiate the client to the add-ons
-Catalog.factory("stremio", ["$http", function($http) {
+Catalog.factory("stremio", ["$http", "$rootScope", function($http, $scope) {
 	var Stremio = require("stremio-addons");
 	var stremio = new Stremio.Client();
 
-	stremio.official = ["http://cinemeta.strem.io", "http://guidebox.strem.io", "http://channels.strem.io", "http://filmon.strem.io"];
+	// Hardcode default official Stremio add-ons - Cinemeta (IMDB metadata), Guidebox (iTunes/Hulu/Netflix/etc. links), Channels (YouTube), Filmon
+	stremio.official = ["http://cinemeta.strem.io/stremioget", "http://guidebox.strem.io/stremioget", "http://channels.strem.io/stremioget", "http://filmon.strem.io/stremioget"];
 	stremio.thirdparty = [];
 
 	var add = stremio.add.bind(stremio);
 	stremio.official.forEach(add);
 
-	// Load add-ons from tracker
+	// Load add-ons from the central tracker
 	$http.get("http://api9.strem.io/addons5").success(function(res) {
 		stremio.official = res.official;
 		stremio.thirdparty = res.thirdparty;
 		res.official.forEach(add); res.thirdparty.forEach(add);
 	}).error(function(er) { console.error("add-ons tracker", er) });
+
+	// VERY important -  update the scope when a new add-on is ready
+	stremio.on("addon-ready", _.debounce(function() { !$scope.$phase && $scope.$apply() }, 300))
 
 	return stremio;
 }]);
@@ -29,19 +33,29 @@ Catalog.factory("stremio", ["$http", function($http) {
 Catalog.factory('Items', [ 'stremio', '$rootScope', '$location', function(stremio, $scope, $location) {
 	var self = { };
 
-	var genres = self.genres = {};
+	var genres = self.genres = { };
 	var items = [];
 
-	stremio.meta.find({ query: { }, limit: 200, skip: 0, complete: true, popular: true, projection: "lean" }, function(err, r) {
-		if (!r) return;
+	// Get all supported types of the stremio addons client - e.g. movie, series, channel
+	var types = [];
+	$scope.$watch(function() { console.log(stremio.supportedTypes);return types = Object.keys(stremio.supportedTypes).sort() }, _.debounce(function() {
+		types.forEach(function(type) {
+			// Query for each type - the add-ons client will automagically decide which add-on to pick for each type
+			stremio.meta.find({ query: { type: type }, limit: 200, skip: 0, complete: true, popular: true, projection: "lean" }, function(err, r, addon) {
+				if (!r) return;
+            	
+            	// Same message as Desktop app
+            	console.log("Discover pulled "+r.length+" items from "+(addon && addon.url));
 
-		items = r;
-		items.forEach(function(x) { 
-			if (! genres[x.type]) genres[x.type] = { };
-			if (x.genre) x.genre.forEach(function(g) { genres[x.type][g] = 1 });
+				items = items.concat(r);
+				items.forEach(function(x) { 
+					if (! genres[x.type]) genres[x.type] = { };
+					if (x.genre) x.genre.forEach(function(g) { genres[x.type][g] = 1 });
+				});
+				$scope.$apply();
+			});
 		});
-		$scope.$apply();
-	});
+	}, 500), true);
 
 	self.all = function() { return items };
 
@@ -53,17 +67,19 @@ Catalog.controller('CatalogController', ['Items', 'stremio', '$scope', '$timeout
 
 	var imdb_proxy = '/poster/';
 
-	self.loading = false;
+	self.loading = false; // TOOD use
 	self.query = '';
 	self.showType = 'movie';
 	self.showGenre = '';
 	self.catTypes = {
 		movie: { name: 'Movies', genres: {} },
-		series: { name: 'TV Shows', genres: {} }
+		series: { name: 'TV Shows', genres: {} },
+		channel: { name: 'Channel', genres: {} },
+		tv: { name: 'TV channels', genres: {} },
+
 	};
 	self.genres = Items.genres;
 
-	$scope.$watch(function() { return Items.loading }, function(loading) { self.loading = loading });
 	$scope.$watchCollection(function() { return [self.showType, self.showGenre, Items.all().length] }, function() {
 		self.items = Items.all().filter(function(x) { 
 			return (x.type == self.showType) && 
