243e0633bd66c836023fecd5d35bc65f4ad4a7c8
Ivo Georgiev <ivo@linvo.org>
2015-12-11 20:50:59 +0200

    Uniform identation

--- public/js/main.js
+++ public/js/main.js
@@ -38,49 +38,49 @@ app.factory("stremio", ["$http", "$rootScope", "$location", function($http, $sco
 app.factory("requests", ["stremio", function(stremio) {	
 	var requests = { };
 	var EventEmitter = require("EventEmitter");
-    requests.candidates = function(args, callback, offlineOnly)
-    {
-        var handle = new EventEmitter(), updated = _.debounce(function() { handle.emit("updated") }, 200);
-        handle.candidates = [];
-        handle.query = angular.copy(args.query);
+	requests.candidates = function(args, callback, offlineOnly)
+	{
+		var handle = new EventEmitter(), updated = _.debounce(function() { handle.emit("updated") }, 200);
+		handle.candidates = [];
+		handle.query = angular.copy(args.query);
 
-        var recv = 0, sent = 0, count = function(fn, id) { 
-            sent++; 
-            return function() { recv++; fn.apply(null, arguments); if (recv === sent) handle.emit("finish"); } 
-        };
+		var recv = 0, sent = 0, count = function(fn, id) { 
+			sent++; 
+			return function() { recv++; fn.apply(null, arguments); if (recv === sent) handle.emit("finish"); } 
+		};
 
-        // If we have a group that firts specifically to the arguments, discard the others ; true to .get as "noPicker"
-        var services = stremio.get("stream.find", args).filter(function(x) { return x.initialized }), 
-            max = (services[0] && services[0].manifest.filter) ? stremio.checkArgs(args, services[0].manifest.filter) : false,
-        services = services.filter(function(x) { return stremio.checkArgs(args, x.manifest.filter) >= max });
+		// If we have a group that firts specifically to the arguments, discard the others ; true to .get as "noPicker"
+		var services = stremio.get("stream.find", args).filter(function(x) { return x.initialized }), 
+			max = (services[0] && services[0].manifest.filter) ? stremio.checkArgs(args, services[0].manifest.filter) : false,
+		services = services.filter(function(x) { return stremio.checkArgs(args, x.manifest.filter) >= max });
 
-        // Group by identifier, and fallthrough within each group
-        args.stremio_rushed = true; // indicate to addons client to try the next one too if we don't get a quick response
-        if (!offlineOnly) _.chain(services).groupBy(function(x) { return x.identifier() }).each(function(group, id) {
-            stremio.fallthrough(group, "stream.find", args, count(function(err, resp, addon) {
-                if (err) console.error(err);
+		// Group by identifier, and fallthrough within each group
+		args.stremio_rushed = true; // indicate to addons client to try the next one too if we don't get a quick response
+		if (!offlineOnly) _.chain(services).groupBy(function(x) { return x.identifier() }).each(function(group, id) {
+			stremio.fallthrough(group, "stream.find", args, count(function(err, resp, addon) {
+				if (err) console.error(err);
 
-                add((Array.isArray(resp) ? resp : []).filter(function(x) { return x }).map(function(x) { 
-                    x.addon = addon;
-                    x._id = x.infoHash || x.url || x.externalUrl || x.yt_id; /// for _.uniq
-                    return x;
-                }));
-            }));
-        }).value();
+				add((Array.isArray(resp) ? resp : []).filter(function(x) { return x }).map(function(x) { 
+					x.addon = addon;
+					x._id = x.infoHash || x.url || x.externalUrl || x.yt_id; /// for _.uniq
+					return x;
+				}));
+			}));
+		}).value();
 
-        function add(resp) {
-            var len = handle.candidates.length, hadFirst = !!handle.first;
-            handle.candidates = _.uniq(handle.candidates.concat(resp || []), function(x) { return x._id });
-            handle.selected = handle.first = handle.candidates[0];
+		function add(resp) {
+			var len = handle.candidates.length, hadFirst = !!handle.first;
+			handle.candidates = _.uniq(handle.candidates.concat(resp || []), function(x) { return x._id });
+			handle.selected = handle.first = handle.candidates[0];
 
-            if (handle.candidates.length > len || !hadFirst) updated();
-            if (handle.first && handle.first.isPlayable && !handle.first.isExternal) handle.emit("viable");
-        };
+			if (handle.candidates.length > len || !hadFirst) updated();
+			if (handle.first && handle.first.isPlayable && !handle.first.isExternal) handle.emit("viable");
+		};
 
-        handle.on("finish", function() { handle.finished = true }); 
+		handle.on("finish", function() { handle.finished = true }); 
 
-        return handle;
-    };
+		return handle;
+	};
 
 	return requests;
 }])
@@ -136,11 +136,11 @@ app.controller('discoverCtrl', ['stremio', '$scope', 'metadata', function mainCo
 	function receiveItems(err, r, addon) {
 		if (err) console.error(err);
 		if (!r) return;
-    	
-    	delayedApply();
+		
+		delayedApply();
 
-    	// Same message as Desktop app
-    	console.log("Discover pulled "+r.length+" items from "+(addon && addon.url));
+		// Same message as Desktop app
+		console.log("Discover pulled "+r.length+" items from "+(addon && addon.url));
 
 		items = items.concat(r.map(function(x) { return new metadata(x) }));
 		items.forEach(function(x) { 
@@ -199,26 +199,26 @@ app.controller('discoverCtrl', ['stremio', '$scope', 'metadata', function mainCo
 	};
 
 
-    // Featured content
-    // This can be done by listening to addon-ready much more easily
-     /*
-    var featuredIds = [];
-    $scope.featured = [];
-    $scope.$watchCollection(function() {
-        stremio.get().forEach(function(s) { 
-            if (s.manifest.featured && s.manifest.featured[stremio.countryCode]) featuredIds.push(s.manifest.featured[stremio.countryCode]);
-            if (s.manifest.featured && s.manifest.featured.ALL) featuredIds.push(s.manifest.featured.ALL);
-        });
-        featuredIds = _.chain(featuredIds).flatten().uniq().value();
-        return featuredIds;
-    }, _.debounce(function() {
-        var metas = [];
-        metadata.retrieveMany(featuredIds, false, function(id, m) { if (m) metas.push(m) }, function() {
-            $scope.featured = metas;
-            metas.forEach(function(x) { x.featured = true });
-            !$scope.$$phase && $scope.$digest();            
-        });
-    }), 400);
+	// Featured content
+	// This can be done by listening to addon-ready much more easily
+	 /*
+	var featuredIds = [];
+	$scope.featured = [];
+	$scope.$watchCollection(function() {
+		stremio.get().forEach(function(s) { 
+			if (s.manifest.featured && s.manifest.featured[stremio.countryCode]) featuredIds.push(s.manifest.featured[stremio.countryCode]);
+			if (s.manifest.featured && s.manifest.featured.ALL) featuredIds.push(s.manifest.featured.ALL);
+		});
+		featuredIds = _.chain(featuredIds).flatten().uniq().value();
+		return featuredIds;
+	}, _.debounce(function() {
+		var metas = [];
+		metadata.retrieveMany(featuredIds, false, function(id, m) { if (m) metas.push(m) }, function() {
+			$scope.featured = metas;
+			metas.forEach(function(x) { x.featured = true });
+			!$scope.$$phase && $scope.$digest();            
+		});
+	}), 400);
 	*/
 
 	return self;
@@ -256,9 +256,9 @@ app.controller('infobarCtrl', ['stremio', '$scope', 'requests', function(stremio
 		else return vid.title;
 	};
 
-    $scope.streamName = function(stream) {
-        return stream.name || (stream.addon && stream.addon.manifest && stream.addon.manifest.name) || (stream.addon && stream.addon.url)
-    };
+	$scope.streamName = function(stream) {
+		return stream.name || (stream.addon && stream.addon.manifest && stream.addon.manifest.name) || (stream.addon && stream.addon.url)
+	};
 }]);
 
 app.controller('')
