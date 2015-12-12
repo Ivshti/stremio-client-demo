0549472e8816f2be79e4e8648dbce24a216857c3
Ivo Georgiev <ivo@linvo.org>
2015-12-12 11:28:43 +0200

    Add-ons: add a UI to load a new add-on

--- public/js/main.js
+++ public/js/main.js
@@ -257,4 +257,9 @@ app.controller('addonsCtrl', ['stremio', '$scope', function(stremio, $scope) {
 	$scope.identifiers = function() {
 		return _.uniq(stremio.get().map(function(x) { return x.identifier() }))
 	};
+
+	$scope.add = function() {
+		stremio.add($scope.addonUrl);
+		$scope.addonUrl = "";
+	}
 }])
