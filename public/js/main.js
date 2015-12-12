c1cad6282ddffd3987f73472cb9fdb163c7da31f
Ivo Georgiev <ivo@linvo.org>
2015-12-12 11:41:07 +0200

    Search: basic search controller

--- public/js/main.js
+++ public/js/main.js
@@ -212,8 +212,57 @@ app.controller('discoverCtrl', ['stremio', '$scope', 'metadata', function mainCo
 	return self;
 }]); 
 
-app.controller('searchCtrl', ['stremio', '$scope', function(stremio, $scope) {
+app.controller('searchCtrl', ['stremio', 'metadata', '$scope', function(stremio, metadata, $scope) {
+	var searchProj = { name: 1, poster: 1, type: 1, popularity: 1, imdbRating: 1, description: 1 };
+	useAsId.forEach(function(id) { searchProj[id] = 1 });
 
+	$scope.selected = { item: null };
+
+	$scope.results = { all: {}, prio: {} }; $scope.groups = [];
+
+	$scope.search = function() { 
+		console.log("search for "+$scope.searchQuery);
+
+		$scope.results = { all: {}, prio: {} }; $scope.groups = [];
+	
+		// get all meta.search-supporting add-ons		
+		stremio.get("meta.search").forEach(function(addon) {
+			addon.call("meta.search", [stremio.getAuth(), { 
+				query: $scope.searchQuery, limit: 10,
+				projection: searchProj
+			}], function(skip, err, error, result) {
+				if (skip) return;
+				if (! (result && result.results)) return;
+
+				var results = result.results.map(function(item) { return new metadata(item) });
+				$scope.results.all[addon.identifier()] = results; // group results by identifier
+				if (results[0] && (results[0].type=="movie" || results[0].type=="series")) $scope.results.prio[results[0].type] = 1; // bump the type of the first result
+				$scope.updateGroups();
+				
+				$scope.$digest();
+			}, true);
+		});
+	};
+	
+	$scope.updateGroups = function() {
+		var all = _.reduce($scope.results.all, function(a,b) { return a.concat(b) }, []);
+		$scope.groups = _.chain(all).groupBy("type")
+			.map(function(items){ return { items: items, type: items[0].type } })
+			.sortBy(function(group) { return $scope.results.prio[group.type] })
+			.value();
+		$scope.noResults = $scope.searchQuery && (all.length == 0);
+
+		console.log($scope.groups)
+	};
+
+	$scope.closeSearch = function() {
+		$scope.searchQuery = ""; 
+		$scope.groups = [];
+		$scope.noResults = false;
+	};
+
+	$scope.$watch("searchQuery", function(s) { if(!s) $scope.closeSearch() });
+	$scope.$watch(function(){ return $scope.view }, function(tab) { $scope.closeSearch() });
 }]);
 
 app.controller('infobarCtrl', ['stremio', '$scope', 'requests', function(stremio, $scope, requests) {
