21f98354242dfbe4d396fdb018de6f3aea2eedd3
Ivo Georgiev <ivo@linvo.org>
2015-12-12 11:32:53 +0200

    Add-ons panel: show type

--- public/js/main.js
+++ public/js/main.js
@@ -262,4 +262,11 @@ app.controller('addonsCtrl', ['stremio', '$scope', function(stremio, $scope) {
 		stremio.add($scope.addonUrl);
 		$scope.addonUrl = "";
 	}
+
+	$scope.getType = function(url) {
+		if (stremio.official.indexOf(url) > -1) return "official";
+		if (stremio.thirdparty.indexOf(url) > -1) return "third party";
+		return "user";
+	}
+
 }])
