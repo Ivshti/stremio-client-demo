f98d90e5c608e0d1a80b2e4f15a00db0df0019ba
Ivo Georgiev <ivo@linvo.org>
2016-09-03 16:04:14 +0300

    selected.item -> selectedItem

--- public/js/controllers/infobar.js
+++ public/js/controllers/infobar.js
@@ -2,25 +2,25 @@
 app.controller('infobarCtrl', ['stremio', '$scope', 'requests', function(stremio, $scope, requests) {
 	// Get all streams for an item; belongs to infobar
 	var delayedDigest = _.debounce(function() { !$scope.$phase && $scope.$digest() }, 300);
-	$scope.$watch(function() { return $scope.selected.item && $scope.selected.item.id }, function() {
+	$scope.$watch(function() { return $scope.selectedItem && $scope.selectedItem.id }, function() {
 		$scope.handle = null;
 		$scope.selected.video = null;
-		if (! $scope.selected.item) return;
+		if (! $scope.selectedItem) return;
 
-		stremio.meta.get({ query: $scope.selected.item.getQuery() }, function(err, fullmeta) {
-			if (fullmeta && $scope.selected.item) { 
-				var p = $scope.selected.item.popularities;
-				_.extend($scope.selected.item, fullmeta);
-				_.extend($scope.selected.item, { popularities: _.extend(p, fullmeta.popularities) }); // merge that prop
+		stremio.meta.get({ query: $scope.selectedItem.getQuery() }, function(err, fullmeta) {
+			if (fullmeta && $scope.selectedItem) { 
+				var p = $scope.selectedItem.popularities;
+				_.extend($scope.selectedItem, fullmeta);
+				_.extend($scope.selectedItem, { popularities: _.extend(p, fullmeta.popularities) }); // merge that prop
 
 				delayedDigest();
 				$scope.selected.video = fullmeta.episodes ? fullmeta.episodes[0] : (fullmeta.uploads ? fullmeta.uploads[0] : null);
 			}
 		});
 	});
-	$scope.$watch(function() { return $scope.selected.item && $scope.selected.item.getQuery($scope.selected.video) }, function(query) {
+	$scope.$watch(function() { return $scope.selectedItem && $scope.selectedItem.getQuery($scope.selected.video) }, function(query) {
 		$scope.handle = null;
-		if (! $scope.selected.item) return;
+		if (! $scope.selectedItem) return;
 		$scope.handle = requests.candidates({ query: query }).on("updated", delayedDigest);
 	}, true);
 
